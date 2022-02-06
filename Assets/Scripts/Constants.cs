a1064db7765497d98084687104afbb0679a13fdc
Alexey Ptitsyn <alexey.ptitsyn@gmail.com>
2022-02-07 00:07:06 +0300

    Added constants storage.

---
+++ Assets/Scripts/Constants.cs
@@ -0,0 +1,7 @@
+﻿// Global-available constants. Such as version number and so on.
+// Copyright Alexey Ptitsyn <alexey.ptitsyn@gmail.com>, 2022
+
+public static class Constants
+{
+    public static string Version = "0.2.1";
+}
