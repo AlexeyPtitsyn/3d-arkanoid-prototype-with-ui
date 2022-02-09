38e2a8352e966e989f05dd0aaa77b73903b6a954
Alexey Ptitsyn <alexey.ptitsyn@gmail.com>
2022-02-10 00:00:20 +0300

    Added text animation on main menu.

--- Assets/Scripts/Controllers/StartScreenController.cs
+++ Assets/Scripts/Controllers/StartScreenController.cs
@@ -22,24 +22,69 @@ namespace Controllers
         [SerializeField]
         private GameObject _textVersion;
 
-        private float _textMoveSpeed = 2f;
+        private float _textMoveSpeed = 10f;
 
-        private float _textArkanoidFrom = -120f;
-        private float _textArkanoidTo = 0f;
+        private float _textArkanoidFrom = 400f;
+        private float _textArkanoidTo = 500f;
         private bool _textArkanoidMoveRight = true;
 
-        private float _text3dFrom = 0f;
-        private float _text3dTo = 360f;
+        private float _text3dFrom = 740f;
+        private float _text3dTo = 880f;
         private bool _text3dMoveRight = false;
 
         private void Awake()
         {
             _textVersion.GetComponent<Text>().text = $"Version: <color=\"#fff\">{Constants.Version}</color>";
 
-
+            StartCoroutine(TextArkanoidMove());
+            StartCoroutine(Text3dMove());
 
             _btnNewGame.GetComponent<Button>().onClick.AddListener(OnNewGame);
             _btnExitGame.GetComponent<Button>().onClick.AddListener(OnExitGame);
+        }
+
+        private void DoMoveText(GameObject obj, ref float from, ref float to, ref bool isMoveRight)
+        {
+            if (isMoveRight)
+            {
+                var comp = obj.GetComponent<RectTransform>();
+                comp.position += new Vector3(1, 0, 0) * Time.deltaTime * _textMoveSpeed;
+
+                if (comp.position.x >= to)
+                {
+                    isMoveRight = false;
+                }
+            }
+            else
+            {
+                var comp = obj.GetComponent<RectTransform>();
+                comp.position += new Vector3(-1, 0, 0) * Time.deltaTime * _textMoveSpeed;
+
+                if (comp.position.x <= from)
+                {
+                    isMoveRight = true;
+                }
+            }
+        }
+
+        IEnumerator TextArkanoidMove()
+        {
+            while(true)
+            {
+                DoMoveText(_textArkanoid, ref _textArkanoidFrom, ref _textArkanoidTo, ref _textArkanoidMoveRight);
+
+                yield return null;
+            }
+        }
+
+        IEnumerator Text3dMove()
+        {
+            while(true)
+            {
+                DoMoveText(_text3d, ref _text3dFrom, ref _text3dTo, ref _text3dMoveRight);
+
+                yield return null;
+            }
         }
 
         private void OnExitGame()
